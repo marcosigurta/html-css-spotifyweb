2fddfdff2bf88405393c3f8034c7f25dc6d41883
marco <marcosigurta.ovb@gmail.com>
2020-06-11 13:52:12 +0200

    Added JS

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById('username').innerHTML = "Username"
+document.getElementById('username-2').innerHTML = "Username"
+document.getElementById('username-2').style.fontSize = "8px"
